c8474a3c4f23e87008a71a99ca066c4c2d3b55b4
Kimjimin97 <sja9707@daum.net>
2023-04-25 13:47:17 +0900

    백준 3584 가장 가까운 공통 조상 찾기

---
+++ beackjoon/3584_가장가까운공통조상.py
@@ -0,0 +1,47 @@
+import sys
+sys.setrecursionlimit(10**5)
+T = int(input())
+graph = []
+first_visited =[]
+
+def dfs(node):
+
+    ## 종료 시점
+    if len(graph[node]) == 0:
+        return
+    
+    ## 부모 노드 찾기
+    parent = graph[node][0]
+
+    ## 만약 이전에 방문한 적이 있다면 두개 노드가 겹치는 포인트
+    if first_visited[parent]:
+        print(parent)
+        return
+    
+    ## 겹치지 않는다면 True 해주기
+    else:
+        first_visited[parent] = True
+        dfs(parent)
+    
+
+
+
+
+
+
+for _ in range(T):
+    n = int(input())
+    graph = [[] for _ in range(n+1)]
+
+    for _ in range(n-1):
+        a,b = map(int, input().split())
+        graph[b].append(a)
+
+    first_visited = [False]*(n+1)
+    node1, node2 = map(int, input().split())
+
+    first_visited[node1] = True
+    dfs(node1)
+    
+    dfs(node2)
+    
